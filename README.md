e716b5a60145d0bb947750df3e463bdd1e7bb38a
katochsenthal <katoch.senthal@gmail.com>
2022-04-17 16:26:29 -0400

    accepting input from users

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-hello!
+ 
+  # sd
+
+  ## 📄 Description
+    ds
+
+  ## 𝌞 Table of Contents
+  sd
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Credits] (#credits)
+  4. [GitHub Username](#gitHub-username)
+  5. [Licenses](#licenses)
+
+  ## 🏁 Installation
+  sd
+
+  ## 🎮 Usage
+  ds
+
+  ## ❤️ Credits
+  undefined
+
+  ## 👩‍💻 GitHub username
+  https://github.com/d
+
+  ## 📜 Licenses
+  MIT,Apache
+
+  ##❓ Questions?
+  sd
+
+  ## 🧪 Test Instructions
+  dsf
+
--- index.js
+++ index.js
@@ -1,88 +1,133 @@
 // packages needed for this application
+const { deepStrictEqual } = require("assert");
+
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = () => {
-  return inquirer.prompt(
-    [
-      {
-        type: "input",
-        name: "project",
-        message: "What is the name of the Project",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("Please enter project");
-            return false;
-          }
-        },
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "Provide a project description",
-      },
-      {
-        type: "input",
-        name: "Table of Contents",
-        message: "Table of Contents",
-      },
-      {
-        type: "input",
-        name: "Installation",
-        message:
-          "What are the steps required to install your project? Provide a step-by-step to run description of how to get the development environment running",
-      },
-      {
-        type: "input",
-        name: "Usage",
-        message:
-          "Provide instructions and examples for use. Provide screenshots or a video url to the project",
-      },
-      {
-        type: "input",
-        name: "Credits",
-        message:
-          "List your collaborators, or any third-party assets that require attribution",
-      },
-      {
-        type: "checkbox",
-        name: "License",
-        message:
-          "Include a license to let other developers know what they can and cannot do with your project",
-        choices: ["MIT", "Apache", "GPL"],
-      },
-      {
-        type: "input",
-        name: "Questions",
-        message: "",
-      },
-    ]
-      .then((answers) => {})
-      .catch((err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(answers);
-        }
-      })
-  );
-};
+// array of questions for user input
 
-questions;
+const questions = [
+  {
+    type: "input",
+    name: "project_title",
+    message: "What is the name of the Project",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter project");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a project description",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Provide project description");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "content",
+    message: "Table of Contents",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "What are the steps required to install your project? Provide a step-by-step to run description of how to get the development environment running",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please installation requirements");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "Provide instructions and examples for use. Provide screenshots or a video url to the project",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Provide screenshots or link to a video");
+        return false;
+      }
+    },
+  },
 
-// TODO: Create a function to write README file
+  {
+    type: "input",
+    name: "Credits",
+    message:
+      "List your collaborators, or any third-party assets that require attribution",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter name of contributors");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github_username",
+    message: "enter your github username",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter your GitHub Username");
+        return false;
+      }
+    },
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message:
+      "Include a license to let other developers know what they can and cannot do with your project",
+    choices: ["MIT", "Apache", "GPL", "ISC", "AGPL", "MPL"],
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        console.log("Please select a license to continue");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "For future questions enter your email?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Instructions on how to test your project",
+  },
+];
 
-fs.writeFile("readme.md", questions(), (err) => {
-  if (err) return console.log(err);
-
-  console.log("successfully wrote in README file!");
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers));
+  fs.writeFile("README.md", generateMarkdown(answers), (err) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("Readme.md file generated!");
+    }
+  });
 });
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
